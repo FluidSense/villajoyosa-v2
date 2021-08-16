b1e296f8b87b49a5f4d1b88abab39d59a812034e
Johannes Kvamme <johannes.kvamme@bekk.no>
2021-08-16 22:39:40 +0200

    Lock ID of title fields

--- cms/deskStructure.ts
+++ cms/deskStructure.ts
@@ -9,6 +9,6 @@ export default () =>
     .items([
       S.listItem()
         .title('Sidetittel')
-        .child(S.document().schemaType('pageTitle').documentId('pageTitle')),
+        .child(S.document().schemaType('pageTitle').documentId('15c71094-5e46-47eb-93b2-3b33dccc5311')),
       ...S.documentTypeListItems().filter(singletonRestrictedPages),
     ]);
