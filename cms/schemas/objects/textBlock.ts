e609345a1ec6f55344080b94acd8a9b532172b14
Johannes Kvamme <johannes.kvamme@bekk.no>
2021-10-12 22:33:29 +0200

    :sparkles: Add support for adding TextWithIcon into block content

--- cms/schemas/objects/textBlock.ts
+++ cms/schemas/objects/textBlock.ts
@@ -1,6 +1,6 @@
 export default {
-  title: 'Textblock',
-  name: 'textblock',
-  type: 'array',
-  of: [{ type: 'block' }],
+  title: "Textblock",
+  name: "textblock",
+  type: "array",
+  of: [{ type: "block" }, { type: "textWithIcon" }],
 };
