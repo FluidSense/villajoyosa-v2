30e6233e75c9b593da5beeeedcb0fb2ca820a415
Johannes Kvamme <johannes.kvamme@bekk.no>
2021-10-14 00:50:44 +0200

    :wrench: Try to remove unnecessary Netlify functions

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  target: 'serverless', // See if this stops creating useless functions https://docs.netlify.com/configure-builds/common-configurations/next-js/
   images: {
     domains: ['https://villajoyosa.kvamme.land/', "cdn.sanity.io"],
   },
-  generateBuildId: () => 'build',
+  generateBuildId: () => 'build', // Should decrease build time on Netlify
 }
