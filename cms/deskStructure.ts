81699d42d304b3f6e9e974a08e6c4cce87188d69
Johannes Kvamme <johannes.kvamme@bekk.no>
2021-10-14 21:15:16 +0200

    :sparkles: Add media browser to sanity

--- cms/deskStructure.ts
+++ cms/deskStructure.ts
@@ -1,16 +1,25 @@
-import S from '@sanity/desk-tool/structure-builder';
-import { MdTitle } from 'react-icons/md';
+import S from "@sanity/desk-tool/structure-builder";
+import { MdTitle } from "react-icons/md";
 
 const singletonRestrictedPages = (listItem) =>
-  !['pageTitle'].includes(listItem.getId());
+  !["pageTitle"].includes(listItem.getId());
+
+const undesiredLibraryDocuments = (listItem) =>
+  !["media.tag"].includes(listItem.getId());
 
 export default () =>
   S.list()
-    .title('Innhold')
+    .title("Innhold")
     .items([
       S.listItem()
-        .title('Sidetittel')
+        .title("Sidetittel")
         .icon(MdTitle)
-        .child(S.document().schemaType('pageTitle').documentId('15c71094-5e46-47eb-93b2-3b33dccc5311')),
-      ...S.documentTypeListItems().filter(singletonRestrictedPages),
+        .child(
+          S.document()
+            .schemaType("pageTitle")
+            .documentId("15c71094-5e46-47eb-93b2-3b33dccc5311")
+        ),
+      ...S.documentTypeListItems()
+        .filter(singletonRestrictedPages)
+        .filter(undesiredLibraryDocuments),
     ]);
