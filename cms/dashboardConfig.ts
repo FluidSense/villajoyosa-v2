ee56dc95af0ff240c3b8347eb8f70b9ef49be40b
Johannes Kvamme <johannes.kvamme@bekk.no>
2021-10-14 22:12:41 +0200

    :sparkles: Add deployment capabilities to studio

---
+++ cms/dashboardConfig.ts
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+    {
+      name: "netlify",
+      options: {
+        title: "Lanser endringer",
+        sites: [
+          {
+            title: "Nettsiden",
+            apiId: "e69024ce-d948-4699-acd1-963be127a2af",
+            buildHookId: "6168884e3900f914279a805d",
+            name: "villajoyosa-v2-web",
+          },
+        ],
+      },
+    },
+  ],
+};
