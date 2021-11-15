d53655ec6e73b538724b560fb160b239d103b8d7
Johannes Kvamme <johannes.kvamme@bekk.no>
2021-11-15 22:24:05 +0100

    :sparkles: Use Swiper for frontpage image carousel

--- web/components/ImageCarousel.tsx
+++ web/components/ImageCarousel.tsx
@@ -1,8 +1,12 @@
 import { FC } from "react";
-import Carousel from "react-responsive-carousel/lib/js/components/Carousel/index";
 import { SanityImage } from "../types/sanityTypes";
-import style from "../styles/ImageCarousel.module.css";
 import { urlFor } from "../urlBuilder";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade } from "swiper";
+
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/effect-fade";
 
 type Props = {
   images: SanityImage[];
@@ -11,27 +15,21 @@ type Props = {
 const ImageCarousel: FC<Props> = (props) => {
   const { images } = props;
   return (
-    <Carousel
-      className={style.carouselWrapper}
-      showArrows={false}
-      autoPlay={true}
-      showIndicators={false}
-      showStatus={false}
-      showThumbs={false}
-      stopOnHover={false}
-      swipeable={false}
-      infiniteLoop={true}
-      transitionTime="3000"
-      animationHandler="fade"
+    <Swiper
+      modules={[Autoplay, EffectFade]}
+      effect="fade"
+      autoplay
+      speed={2500}
     >
       {images.map((image) => (
-        <img
-          key={image.name}
-          src={urlFor(image.imageUrl).url()}
-          alt={image.name}
-        />
+        <SwiperSlide key={image.name} style={{ height: "200px" }}>
+          <img
+            src={urlFor(image.imageUrl).height(200).url()}
+            alt={image.name}
+          />
+        </SwiperSlide>
       ))}
-    </Carousel>
+    </Swiper>
   );
 };
 
--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  target: 'serverless', // See if this stops creating useless functions https://docs.netlify.com/configure-builds/common-configurations/next-js/
   images: {
     domains: ['https://villajoyosa.kvamme.land/', "cdn.sanity.io"],
   },
