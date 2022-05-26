100a173b37f2cbb6dbc2237ddb68e3e9f6a13353
Johannes Kvamme <johannes.kvamme@bekk.no>
2022-05-26 22:07:14 +0200

    :chart_with_upwards_trend: Add splitbee tracking

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,6 +2,9 @@ import "../styles/globals.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "leaflet/dist/leaflet.css";
 import React from "react";
+import splitbee from "@splitbee/web";
+
+splitbee.init();
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
